9b6e8a93b35e5e2ebeae2b18b5a592762ba1b037
gemaProg <gema.garciafernandez@educa.madrid.org>
2023-02-06 20:10:06 +0100

    HolaMundo

---
+++ Ej1HelloWorld/src/main/java/com/example/ej1helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ej1helloworld;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
