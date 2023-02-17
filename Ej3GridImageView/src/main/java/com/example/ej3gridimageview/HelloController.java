eccdb9a7343ccbae5a501f8df6669cc3a7ea59e5
gemaProg <gema.garciafernandez@educa.madrid.org>
2023-02-17 02:44:57 +0100

    Grid con listener en sus 64 pane, para poderlo mover sin problemas en el layout.

--- Ej3GridImageView/src/main/java/com/example/ej3gridimageview/HelloController.java
+++ Ej3GridImageView/src/main/java/com/example/ej3gridimageview/HelloController.java
@@ -25,15 +25,15 @@ public class HelloController implements Initializable {
         label.setText("Welcome to JavaFX Application!");
     }
 
-    public void click(MouseEvent mouseEvent) {
+    /*public void click(MouseEvent mouseEvent) {
         int columna = 0;
         int fila = 0;
-        /*for (Node node : mainGrid.getChildren()) {
+        for (Node node : mainGrid.getChildren()) {
             if (node.getBoundsInParent().contains(mouseEvent.getSceneX(), mouseEvent.getSceneY())) {
                 columna = GridPane.getColumnIndex(node);
                 fila = GridPane.getRowIndex(node);
             }
-        }*/
+        }
         boolean encontrado=false;
         for (int i=0; i< mainGrid.getChildren().size()  && !encontrado;i++) {
             Node node = mainGrid.getChildren().get(i);
@@ -50,11 +50,23 @@ public class HelloController implements Initializable {
             columna = GridPane.getColumnIndex(node);
             fila = GridPane.getRowIndex(node);
             System.out.println("Row : " + fila + ", Col : " + columna);
-        }*/
-    }
+        }
+    }*/
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        pintarTablero();
+    }
+    public void accion(String coordenadas){
+        String args[] = coordenadas.split(";");
+        int fila = Integer.parseInt(args[0]);
+        int columna = Integer.parseInt(args[1]);
+        System.out.println(fila+"-"+columna);
+    }
+    public void accion(int x, int y){
+        System.out.println(x+"-"+y);
+    }
+    private void pintarTablero() {
         Pane pane;
         for (int i = 0; i <= 7; i++) {
             for (int j = 0; j <= 7; j++) {
@@ -67,11 +79,23 @@ public class HelloController implements Initializable {
                 if (i == j) {
                     //addAll vs add
                     pane.getChildren().add(new ImageView(new Image("File:src/main/resources/com/example/ej3gridimageview/imagenes/CaballoBlanco.png")));
+
+                    //pane.getChildren().add(new ImageView(new Image("File:src/main/resources/com/example/ej3gridimageview/imagenes/CaballoBlanco.png")));
                 }
                 mainGrid.add(pane, j, i);
+                String message = "Click on cell ["+i+", "+j+"]";
+                String envio = i+";"+j;
+                int fila = i;
+                int columna = j;
+                pane.setOnMouseClicked(e -> {
+                    System.out.println(message);
+                    //accion(envio);
+                    accion(fila,columna);
+                });
             }
         }
     }
+
 
     public void enroque(ActionEvent actionEvent) {
     }
