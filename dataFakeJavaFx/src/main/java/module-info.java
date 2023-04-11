8249f5f065925f063178e183d6c595d1345ffa2e
gemaProg <gema.garciafernandez@educa.madrid.org>
2023-04-11 21:08:01 +0200

    ViewModel ObservableList

--- dataFakeJavaFx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxfaker2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires datafaker;
-
-    opens com.example.javafxfaker2 to javafx.fxml;
-    exports com.example.javafxfaker2;
-}
