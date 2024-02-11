4bff29a310dd87d7bb86bd3d3e530458716e5ea5
gemaProg <gema.garciafernandez@educa.madrid.org>
2024-02-11 23:35:18 +0100

    Actualizado hasta este proyecto

--- Ej2AlertCombo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.ej2alertcombo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.ej2alertcombo to javafx.fxml;
-    exports com.example.ej2alertcombo;
-}
